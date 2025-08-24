30af707390c259d2b17c982e2566fd8946f7fbd2
Zach Kimberg <zachary@kimberg.com>
2025-08-23 18:08:16 -0700

    Tests for class construction choices

---
+++ tests/test_func_init.py
@@ -0,0 +1,39 @@
+import pychoice as choice
+
+# Define functions
+
+class Foo:
+    def foo(self) -> str:
+        return "foo"
+
+@choice.func()
+def cfoo() -> Foo:
+    return Foo()
+
+def test_foo():
+    assert cfoo().foo() == "foo"
+
+# Using a class as an implementation
+
+@choice.func(implements=cfoo)
+class Bar(Foo):
+    def foo(self) -> str:
+        return "bar"
+
+def test_bar():
+    assert cfoo().foo() == "bar"
+
+choice.rule([test_bar, cfoo], Bar)
+
+@choice.func(implements=cfoo, args=["arg"])
+class ArgBar(Foo):
+    def __init__(self, arg: str):
+        self.arg = arg
+
+    def foo(self) -> str:
+        return f"bar-{self.arg}"
+
+def test_arg_bar():
+    assert cfoo().foo() == "bar-arg"
+
+choice.rule([test_arg_bar, cfoo], ArgBar, arg="arg")
